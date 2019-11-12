7f0752387dfb3e20d54406db143dd54910df43da
Neil Campbell <neildcampbell@hotmail.com>
2019-11-12 15:19:48 +0000

    added exit

--- spec/echoSpec.js
+++ spec/echoSpec.js
@@ -1,12 +1,36 @@
 describe("Echo Process Review ", function(){
+    let date;
+    let firstDate;
+    let time;
+    let timeArray;
+    beforeEach(function(){
+        firstDate = new Date()
+        date = firstDate.toLocaleDateString()
+        time = firstDate.toTimeString()
+        timeArray = time.split(" ")
+        console.log(date)
+        console.log(time)
+        console.log(timeArray)
+    })  
+    
     it("should return the input", function(){
         let input = "hello"
         let result = echo(input);
-        expect(result).toEqual(input)
+        expect(result).toEqual(input + " " + date + " " + timeArray[0])
     })
     it("should return the input", function(){
         let input = "hi"
         let result = echo(input);
-        expect(result).toEqual(input)
+        expect(result).toEqual(input + " " + date + " " + timeArray[0])
+    })
+    it("should return the input with the date", function(){
+        let input = "hi"
+        let result = echo(input);
+        expect(result).toEqual(input + " " + date + " " + timeArray[0])
+    })
+    it("should return the Goodbye if exit is entered", function(){
+        let input = "exit"
+        let result = echo(input);
+        expect(result).toEqual('Goodbye!')
     })
 })
--- src/echo.js
+++ src/echo.js
@@ -1,3 +1,12 @@
 function echo(string){
-    return string
+    if(string != 'exit'){
+    firstDate = new Date()
+    date = firstDate.toLocaleDateString();
+    time = firstDate.toTimeString()
+    timeArray = time.split(" ")
+    console.log(string + " " + date + " " + timeArray[0])
+    return string + " " + date + " " + timeArray[0]
+    } else {
+        return 'Goodbye!'
+    }
 }
